674322431e877a28ff3fbb6ffc1b02e0b54e6a30
aditisah <sditi.sah1@gmail.com>
2022-06-21 16:47:35 +0530

    new update

---
+++ src/controllers/blogController.js
@@ -0,0 +1,16 @@
+const blogModel = require('../models/blogModel')
+
+const createBlog = async function(req,res){
+    try{
+        let newBlogEntry = req.body;
+        let newBlog = blogModel(newBlogEntry);
+        res.status(201).send({msg: newBlog});
+       }
+       catch(err){
+        res.status(500).send({Error:err.message})
+       }
+    }
+    
+
+
+module.exports.createBlog = createBlog;
---
+++ src/models/blogModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+const blogModel = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  author_id:{
+    type: ObjectId,
+    ref: "Author",
+    required: true
+  },
+  tags:{
+    type:[String],
+    category: String,
+    required: true
+  },
+  subcategory: {
+    type: [String]
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  },
+  isPublished: {
+    type: Boolean,
+    default: false
+  }
+},{ timestamps: {createdAt: 'created_at', updatedAt: 'updated_at', deletedAt: 'deleted_at',publishedAt: 'published_at'} })
+
+module.exports.blogModel = blogModel
