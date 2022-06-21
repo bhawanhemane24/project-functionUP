dda843881ddef5c9f675e134f61fa171340875e1
unknown <neerajarao2898@gmail.com>
2022-06-21 17:02:50 +0530

    authormodel update

---
+++ src/models/authorModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const authorSchema =new mongoose.Schema({
+     fname: { 
+        type : String ,
+        require : true } ,
+    lname: { 
+        type : String ,
+        require : true },
+     title: { 
+        type : String ,
+        require : true , 
+        enum : ["Mr", "Mrs", "Miss"] }, 
+    email: { 
+        type : String ,
+        require : true ,
+        unique : true},
+    password: {
+        type : String ,
+        require : true }
+    } , {timestamps : true} ) ;
+
+    module.exports = mongoose.model('Proauthor', authorSchema)
